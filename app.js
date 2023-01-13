2e2c30455857947a801a50030596ad734ba002db
Danila <danila.kuzya2001@gmail.com>
2023-01-13 16:35:51 +0300

    MongoDb and start of rutes

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 const express = require("express")
 const config = require("config")
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 
+app.use("/api/auth")
+
 const PORT = config.get('PORT') || 5000;
-app.listen(PORT, () => console.log(`app has been started on port: ${PORT}`))
+
+const uri = config.get("mongoUri")
+
+async function start (){
+    try {
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+        client.connect(err => {
+            const collection = client.db("test").collection("devices");
+            client.close();
+        });
+        app.listen(PORT, () => console.log(`app has been started on port: ${PORT}`))
+    } catch (e) {
+        console.log("Server Error", e.message)
+        process.exit(1)
+    }
+}
+
+//
+// async function start (){
+//     try {
+//         mongoose.set("strictQuery", false);
+//         await mongoose.connect(config.get("mongoUri"), () => {
+//             console.log("Connect DB")
+//         });
+//         app.listen(PORT, () => console.log(`app has been started on port: ${PORT}`))
+//     } catch (e) {
+//         console.log("Server Error", e.message)
+//         process.exit(1)
+//     }
+// }
+
+start();
